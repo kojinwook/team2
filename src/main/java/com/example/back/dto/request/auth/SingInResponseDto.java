4f180aae7d2baf5a609dead2eaa61119f54eece5
kojinwook <gojinug4@gmail.com>
2024-06-04 15:50:39 +0900

    dasd

--- src/main/java/com/example/back/dto/request/auth/SingInResponseDto.java
+++ src/main/java/com/example/back/dto/request/auth/SingInResponseDto.java
@@ -1,4 +1,21 @@
 package com.example.back.dto.request.auth;
 
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class SingInResponseDto {
+    @NotBlank
+    private String userId;
+
+    @NotBlank
+    private String password;
+
+
+
 }
